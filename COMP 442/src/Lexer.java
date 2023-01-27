2639ba3bd45aa26b37681babdd04b9111c897671
Jonathan Jong <60296341+jongjonathan@users.noreply.github.com>
2023-01-27 17:32:40 -0500

    Added rest of operators and punctuations

--- COMP 442/src/Lexer.java
+++ COMP 442/src/Lexer.java
@@ -33,6 +33,7 @@ public class Lexer {
                         System.out.println("lambda");
                     }
                     else if((char)nextCharPoint == ' '){
+                        //validate if prev token is good and create the normal equals
                         System.out.println("Normal equals");
                     }
                     else{
@@ -99,7 +100,7 @@ public class Lexer {
                         prevCharPoint = "//";
                        //while loop for the comment section
 
-                        while((nextCharPoint = Fin.read())!= '\n' && (nextCharPoint = Fin.read())!= '\r'){
+                        while((nextCharPoint = Fin.read())!= '\n' && (nextCharPoint)!= '\r'){
                             prevCharPoint +=(char)nextCharPoint;
                         }
                         System.out.println("inline comment " + prevCharPoint);
@@ -137,6 +138,84 @@ public class Lexer {
                         System.out.println("normal forward slash no space");
                     }
                 }
+                //---------- PERIOD -----------
+                else if((char)charPoint == '.'){
+                    boolean isID = isTokenID(prevCharPoint);
+
+                    if(isID == true){
+                        //create token for ID alphanum
+                    }
+                    else{
+                        prevCharPoint += '.';
+                        while(((nextCharPoint = Fin.read() )>= 48 && (nextCharPoint <= 57)) || (char)nextCharPoint == 'e'|| (char)nextCharPoint == '+' ||(char)nextCharPoint == '-'){
+                            prevCharPoint += (char)nextCharPoint;
+                        }
+                        boolean isFloat = isTokenFloat(prevCharPoint); //checks for multiple e, +, -
+                        if(isFloat){
+                            //create token with prevCharPoint as its now the full float ex: 123.123
+                            //reset prevCharPoint
+                            prevCharPoint = ""+nextCharPoint; //if 123.123; then prevCharPoint = ';'
+                        }
+
+                    }
+
+
+
+                }
+                else if((char)charPoint =='*'){
+
+                    //validate token before, if good then create token for before, create token for multiply
+                    System.out.println("multiply");
+                }
+                else if((char)charPoint =='+'){
+                    //validate token before, if good then create token for before, create token for add
+                    System.out.println("add");
+                }
+                else if((char)charPoint =='-'){
+                    //validate token before, if good then create token for before, create token for subtract
+                    System.out.println("subtract");
+                }
+                else if((char)charPoint ==','){
+                    //validate token before, if good then create token for before, create token for comma
+                    System.out.println("comma");
+                }
+                else if((char)charPoint ==';'){
+                    //validate token before, if good then create token for before, create token for semicolon
+                    System.out.println("semicolon");
+                }
+                else if((char)charPoint =='('){
+                    //validate token before, if good then create token for before, create token for openbracket
+                    System.out.println("open bracket");
+                }
+                else if((char)charPoint ==')'){
+                    //validate token before, if good then create token for before, create token for closed bracket
+                    System.out.println("close bracket");
+                }
+                else if((char)charPoint =='{'){
+                    //validate token before, if good then create token for before, create token for curly open bracket
+                    System.out.println("curly open bracket");
+                }
+                else if((char)charPoint =='}'){
+                    //validate token before, if good then create token for before, create token for curly open bracket
+                    System.out.println("curly close bracket");
+                }
+                else if((char)charPoint =='['){
+                    //validate token before, if good then create token for before, create token for curly open bracket
+                    System.out.println("open square bracket");
+                }
+                else if((char)charPoint ==']'){
+                    //validate token before, if good then create token for before, create token for curly open bracket
+                    System.out.println("close square bracket");
+                }
+
+                //for last remaining closing character
+                if(prevCharPoint.equals(';') || prevCharPoint.equals(')') || prevCharPoint.equals('}')|| prevCharPoint.equals(']')){
+                    //make token for it
+                    prevCharPoint = "";
+                }
+
+
+
 
             }
 
@@ -146,7 +225,28 @@ public class Lexer {
             System.out.println(e);
         }
 
+
+
     }
+    public boolean isTokenID(String ID){
+        //alphanum
+        Boolean b = false;
+        return b;
+    }
+    public boolean isTokenFloat(String fl){
+        Boolean b = false;
+        return b;
+    }
+    public boolean isInt(String i){
+        Boolean b = false;
+        return b;
+    }
+    public boolean isResWord(String res){
+        Boolean b = false;
+        return b;
+    }
+
+
 
 
 
